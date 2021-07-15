57830dc795f8f9e1f2f75be2ca810dbacdcd6d66
Tim Dugan <tadugan@me.com>
2021-07-15 13:54:24 -0500

    Laid out some code comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,12 +4,14 @@ import './App.css';
 
 function App() {
 
+  // NOTE: Put Axios function in their respective components
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+      {/* TODO: Setup Routes here*/}
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+// import redux
 
+
+// Reducers go here:
+
+// storeInstance goes here
+
+
+
+// TODO: add Provider to App
 ReactDOM.render(<App />, document.getElementById('root'));
