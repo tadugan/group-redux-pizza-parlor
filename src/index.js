aa4bb35afa8d4b3c7731c6497ca996e035224f0a
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-07-15 15:10:54 -0400

    import redux, pizza reducer added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 // import redux
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
 
 // Reducers go here:
+
+const pizzaReducer =(state = [], action) => {
+    if(action.type === 'GET_PIZZA') {
+        return [
+            ...action.payload,
+        ]
+    }
+    return state;
+}
 
 // storeInstance goes here
 
