f3648781ec6cc4b0a0f8190c5527fb23fa0f9375
Tim Dugan <tadugan@me.com>
2021-07-15 22:46:59 -0500

    installed new components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 // Import Components
 import Checkout from '../Checkout/Checkout';
@@ -10,16 +11,28 @@ function App() {
   // NOTE: Put Axios function in their respective components
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-      {/* TODO: Setup Routes here*/}
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-      <Checkout />
-      <AdminOrder />
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Prime Pizza</h1>
+        </header>
+        {/* <img src='images/pizza_photo.png' />
+        <p>Pizza is great.</p> */}
+        <Route path="/" exact>
+            <p>Step 1:</p>
+            {/* <OrderPizza /> goes here */}
+        </Route>
+        <Route path="/customer" exact>
+            <p>Step 2:</p>
+        </Route>
+        <Route path="/checkout" exact>
+            <Checkout />
+        </Route>
+        <Route path="/admin" exact>
+            <AdminOrder />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
