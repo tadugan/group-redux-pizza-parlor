3c4aaddc01ec1300aa490c9fd41812d56b482f38
Pouapheng Yang <phewy06@yahoo.com>
2021-07-16 11:43:18 -0500

    customer info filled

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -1,30 +1,70 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import axios from "axios";
-import { Dispatch } from "react";
+import { dispatch } from "react";
 import { useDispatch } from "react-redux";
+import { props } from "bluebird";
 
 
 function CustomerInfo() {
     
     const dispatch = useDispatch();
 
-    let [currentCustomer, setCurrentCustomer] = useState({});
+    // useEffect(() => {
+    //     getCustomerInfo();
+    // }, [])
 
-    useEffect(() => {
-        console.log('in useEffect')
-        getCustomerInfo();
-    }, [])
+    onInputChange = (input) => (event) => {
+        this.setState({
+            [input]: event.target.value,
+        });
+    };
 
+    onNextClick = (event => {
+        this.props.dispatch({
+            type: 'SET_CUSTOMER_INFO', payload: this.state});
+    })
 
     const addCustomer = (newCustomer) => {
-        axios({
-            method: 'POST',
-            url: '/orders',
-            data: {id: newCustomer}
-        }).then((response) => {
-            console.log(response);
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
+        orderTestData = {
+        pizzas: [],
+        customer_name: 'John',
+        street_address: '555 Applewood Lane',
+        city: 'Minneapolis',
+        state: 'MN',
+        zip: '55555',
+        type: 'deliver',
+        total: '20'
+        }
+
+    };
+
+    return (
+        <div>
+            <h1>Step 2: Customer Info</h1>
+            <input 
+                type="text"
+                onChange={this.onInputChange(customer_name)}
+                placeholder="Enter Name"
+                />
+            <input
+                type="text"
+                onChange={this.onInputChange(street_address)}
+                placeholder="Enter Address"
+                />
+            <input 
+                type="text"
+                onChange={this.onInputChange(city)}
+                placeholder="Enter City"
+                />
+            <input
+                type="text"
+                onChange={this.onInputChange(zip)}
+                placeholder="Enter Zip Code" 
+                />   
+                <button onClick={this.onNextClick}>Next</button>
+        </div>
+    );
 }
+
+
+export default CustomerInfo;
--- src/index.js
+++ src/index.js
@@ -7,37 +7,61 @@ import logger from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
+// Test data for order
+const orderTestData = {
+    pizzas: [
+        {id: 1, quantity: 2 }, 
+        {id: 2, quantity: 3 },
+        {id: 3, quantity: 5 }
+    ],
+    customer_name: 'Tim',
+    street_address: '777 Oasis Lane',
+    city: 'Minneapolis',
+    state: 'MN',
+    zip: '55557',
+    type: 'Delivery',
+    total: 20
+}
 
-// Reducers go here:
-
-
-// Reducer to keep pizzas in global state
-const pizzaReducer =(state = [], action) => {
-    if(action.type === 'GET_PIZZA') {
-        return [
-            ...state, action.payload,
-        ]
-    }
-    return state;
+const initialState = {
+    pizzas: []
 }
 
 // Reducer to keep order in global state
-const orderReducer = (state = [] action) => {
+const orderReducer = (state = initialState, action) => {
     if(action.type === 'ADD TO CART') {
         return [
             ...state, action.payload,
         ]
+    }
+    else if (action.type === 'ADD_PIZZA') {
+        console.log('in ADD_PIZZA');
+        let allPizzas = state.pizzas;
+        allPizzas.push(action.payload);
+        return state, {pizzas: allPizzas};
+    }
     return state;
 }
 
 // storeInstance goes here
-const storeInstance = createStore{
+const storeInstance = createStore(
     combineReducers({
-        pizzaReducer,
         orderReducer
     }),
     applyMiddleware(logger),
-}
+);
 
 // TODO: add Provider to App
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+
+
+// Data Model for orderReducer state
+// orderTestData = {
+//     pizzas: [],
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     street: '555 Applewood Lane'
+//     city: 'Minneapolis',
+//     state: 'MN',
+//     zip: '55555'
+// }
